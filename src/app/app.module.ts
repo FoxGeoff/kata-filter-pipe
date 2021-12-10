d1097dccfa24d20ba97bc812c5ae9c62a9e24e4a
Geoff Fox <fox.geoffrey@gmail.com>
2021-12-10 10:39:39 -0800

    Step 1 – Create a filter “pipe” to Filter List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FilterPipePipe } from './filter-pipe.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule
