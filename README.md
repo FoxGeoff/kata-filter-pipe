ba0ce2df1de7913b8b97aa3c94ec10c0832d8533
Geoff Fox <fox.geoffrey@gmail.com>
2021-12-10 10:41:18 -0800

    Step 2 - Include filter in main.ts file

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 1. Online ref.: <https://www.freakyjolly.com/angular-typescript-create-filter-list-with-check-boxes-to-select-from-list/>
 
 ### Task: Step 1 – Create a filter “pipe” to Filter List
+
+### Task: Step 2 - Include filter in main.ts file
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FilterPipePipe } from './filter-pipe.pipe';
 import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterPipePipe,
     FilterPipe
   ],
   imports: [
