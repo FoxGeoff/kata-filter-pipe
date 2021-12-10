ff948488b7b7469348cfafd925805de749027ce7
Geoff Fox <fox.geoffrey@gmail.com>
2021-12-10 11:11:03 -0800

    Step 3 - Let's create an app.ts file, which will have important logical functions explained in the feature section above

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ### Task: Step 1 – Create a filter “pipe” to Filter List
 
 ### Task: Step 2 - Include filter in main.ts file
+
+### Task: Step 3 - Let's create an app.ts file, which will have important logical functions explained in the feature section above
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,90 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kata-filter-pipe';
+  name: string;
+  searchText: string = "";
+  selected_count: number = 0;
+
+
+  // Data Object to List Games
+  games = [
+    {
+      name: 'Chess',
+      id: 1,
+      selected: true
+    },
+    {
+      name: 'Ludo',
+      id: 2,
+      selected: false
+    },
+    {
+      name: 'Snakes & Ladders',
+      id: 3,
+      selected: false
+    },
+    {
+      name: 'Carrom',
+      id: 4,
+      selected: false
+    },
+    {
+      name: 'Scrabble',
+      id: 5,
+      selected: false
+    },
+    {
+      name: 'Monopoly',
+      id: 6,
+      selected: true
+    },
+    {
+      name: 'Uno',
+      id: 7,
+      selected: false
+    }
+  ]
+  selected_games: { name: string; id: number; selected: boolean; }[];
+
+  // Getting Selected Games and Count
+  getSelected() {
+    this.selected_games = this.games.filter(s => {
+      return s.selected;
+    });
+    this.selected_count = this.selected_games.length;
+    //alert(this.selected_games);
+  }
+
+  // Clearing All Selections
+  clearSelection() {
+    this.searchText = "";
+    this.games = this.games.filter(g => {
+      g.selected = false;
+      return true;
+    });
+    this.getSelected();
+  }
+
+  //Delete Single Listed Game Tag
+  deleteGame(id: number) {
+    this.searchText = "";
+    this.games = this.games.filter(g => {
+      if (g.id == id)
+        g.selected = false;
+
+      return true;
+    });
+    this.getSelected();
+  }
+
+  //Clear term types by user
+  clearFilter() {
+    this.searchText = "";
+  }
+
+  constructor() {
+    this.name = `Angular! v${VERSION.full}`;
+    this.getSelected();
+  }
+
 }
